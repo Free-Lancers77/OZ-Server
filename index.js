29b6b23d4b79c5c5af4e59bde14088f5f8ccd48a
Hussien320 <hussienzoughaib@gmail.com>
2024-09-07 13:42:50 +0300

    git commit "a";

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 import express from "express";
-import connectDB from "./config/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+import { startServer} from "./controllers/serverfunction.js";
 
 app.use(express.json());
 
-async function startServer() {
-  try {
-    await connectDB();
-    console.log("Database connection established successfully.");
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to connect to the database. Server not started.", error);
-    process.exit(1); // Exit the process with a failure code
-  }
-}
 
 startServer();
